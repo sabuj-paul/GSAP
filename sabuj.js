34f5ffb8c8d88a83fdcac0d4fc2fb6a205a43129
Sabuj Paul <sabujp514@gmail.com>
2026-03-04 23:36:58 +0600

    Add files via upload

---
+++ sabuj.js
@@ -0,0 +1,22 @@
+// This line prints the message "Hello, JavaScript!" to the console
+console.log("Hello, JavaScript!");
+
+
+console.log("roammate er bichi choto")
+
+let a = "roammate er bicchi choto";
+ console.log(a);
+
+ for(let i=0; i< 10; i++){
+    console.log(`roammate er bicchi---${i}`)
+ }
+ function calculateArea(radius) {
+  const pi = 3.14159; // A constant for the value of pi
+  let area = pi * radius * radius; // Calculates the area
+  return area;
+}
+
+let radius = 5;
+let area = calculateArea(radius);
+console.log("The area of the circle is:", area); // Output: The area of the circle is: 78.53975
+
---
+++ script.js
@@ -0,0 +1,85 @@
+gsap.to("#box1",{
+    x: 1000,
+    duration: 2,
+    delay: 1,
+    margin: 10,
+    rotate:360,
+    backgroundColor:"blue",
+    borderRadius:"50%",
+    scale:1.5,
+    repeat:1
+})
+gsap.from("#box2",{
+    x:1000,
+    duration: 2,
+    delay: 1,
+    margin: 10,
+    scale:.5,
+    borderRadius:"150px",
+    repeat:-1,
+    yoyo:true
+})
+gsap.from("h1",{
+    color:"red",
+    opacity:0,
+    delay:1,
+    y:"40px",
+    duration:2,
+    stagger:-.5,
+    
+})
+// gsap.to("#box3",{
+//     x:1500,
+//     rotate:360,
+//     duration:1.5,
+//     delay:1
+// })
+// gsap.to("#box4",{
+//     x:1500,
+//     backgroundColor:"yellow",
+//     rotate:360,
+//     duration:1.5,
+//     delay:2.5
+// })
+// gsap.to("#box5",{
+//     x:1500,
+//     backgroundColor:"green",
+//     scale:0.5,
+//     rotate:360,
+//     duration:1.5,
+//     delay:4
+// })
+
+var tl = gsap.timeline();
+
+tl.to("#box3",{
+    x:1500,
+    rotate:360,
+    duration:1.5,
+    delay:1
+})
+tl.to("#box4",{
+    x:1500,
+    rotate:360,
+    duration:1.5
+})
+tl.to("#box5",{
+    x:1500,
+    rotate:360,
+    duration:1.5
+   
+})
+var tl2 = gsap.timeline();
+
+tl2.from("h2",{
+    y:-30,
+    opacity:0,
+    duration: 1,
+    delay:0.5
+})
+tl2.from("h4",{
+    y:-30,
+    opacity:0,
+    duration: 1,
+    stagger: .5
+})
